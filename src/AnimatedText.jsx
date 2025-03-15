9d879031b276ce0cbfa4e38ca44257897f063da9
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-15 09:24:51 +0100

    fixed infinity

--- src/AnimatedText.jsx
+++ src/AnimatedText.jsx
@@ -11,25 +11,25 @@ const AnimatedText = () => {
     gsap.to(textRef.current, {
       motionPath: {
         path: [
-          { x: -100, y: 50 },
-          { x: 0, y: -50 },
-          { x: 100, y: 50 },
-          { x: 0, y: 0 }
+          { x: -100, y: 50 },  // Left loop
+          { x: 0, y: -50 },    // Middle top
+          { x: 100, y: 50 },   // Right loop
+          { x: 0, y: 0 }       // Back to center
         ],
         curviness: 1.5,
         autoRotate: false
       },
-      duration: 4,
+      duration: 6, // Adjust speed
       repeat: -1,
       ease: "power1.inOut"
     });
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px", position: "relative", height: "200px" }}>
-      <h1 ref={textRef} style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }}>
-        🚀 Welcome this is Stormmaze - WEBSITE IS UNDER MAINTENANCE 🚀
-      </h1>
+    <div style={{ textAlign: "center", marginTop: "50px", position: "relative", height: "200px", overflow: "hidden" }}>
+      <div ref={textRef} style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)", whiteSpace: "nowrap", fontSize: "2rem", fontWeight: "bold" }}>
+        🚀 Welcome this is Stormmaze - WEBSITE IS UNDER MAINTENANCE 🚀 Welcome this is Stormmaze - WEBSITE IS UNDER MAINTENANCE 🚀
+      </div>
     </div>
   );
 };
