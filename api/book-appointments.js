b221772d55c64205f4844f03f29db3589402558b
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-16 22:32:33 +0100

    second working version

--- api/book-appointments.js
+++ api/book-appointments.js
@@ -4,9 +4,13 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
+  }
+
+  if (req.method === "GET" && req.url === "/api/book-appointments/test") {
+    return res.status(200).json({ success: true, message: "API test route is working!" });
   }
 
   if (req.method !== "POST") {
