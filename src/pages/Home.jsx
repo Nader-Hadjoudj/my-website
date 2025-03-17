fc38e679379d9483d2aceb783ba8e37b7eb1c086
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-17 22:02:55 +0100

    iported

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import AnimatedText from "../components/AnimatedText";
+import { AppWithVideoBackground } from "./components/FarmingHeroSection";
 import AppointmentBooking from "../components/AppointmentBooking";
 
 function Home() {
