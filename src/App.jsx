4c30426106b6be15b60acb140f5351eceb95d7b9
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-19 00:27:29 +0100

    added catalog

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/catalogue" element={<Contact />} />
         </Routes>
       <Footer /> {/* Add Footer Here */}
     </Router>
---
+++ src/pages/Catalogue.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import AnimatedText from "../components/AnimatedText";
+import FarmingHeroSection from "../components/FarmingHeroSection";
+import AppointmentBooking from "../components/AppointmentBooking";
+import FarmingProductsShowcase from "../components/FarmingProductsShowcase";
+
+function Home() {
+  return (
+    <div>
+      <AnimatedText />
+      <FarmingHeroSection />
+      <FarmingProductsShowcase />
+    </div>
+  );
+}
+
+export default Home;
