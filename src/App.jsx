9f8384df711b032180688745b4962a8179d08e65
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-18 20:24:51 +0100

    nav bar fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ function App() {
   return (
     <Router>
       <Navbar /> {/* Navbar remains visible on all pages */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <MainContent>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        </MainContent>
       <Footer /> {/* Add Footer Here */}
     </Router>
   );
