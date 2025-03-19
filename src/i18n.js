b827d97c767a9653b0a7983a0210093f464e3304
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-19 22:27:14 +0100

    fix

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslations from './src/locales/en.json';
-import frTranslations from './src/locales/fr.json';
+import enTranslations from './src/locales/en/translation.json';
+import frTranslations from './src/locales/fr/translation.json';
 
 i18n
   .use(initReactI18next)
