8cc65728ff3c7f3312276a2ece63924559bb6b86
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-21 20:31:17 +0100

    chat gpt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   return (
     <div>
       <StormmazeLanding />
-      <AgricultureImportLanding />
+      
       <FarmingHeroSection />
       <FarmingProductsShowcase />
     </div>
