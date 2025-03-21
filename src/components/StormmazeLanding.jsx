15b017a5bc3a2e787e242bd75a196715ec8ca72c
Nader Hadjoudj <nader.hadjoudj@ecole.ensicaen.fr>
2025-03-21 21:15:10 +0100

    with picture

--- src/components/StormmazeLanding.jsx
+++ src/components/StormmazeLanding.jsx
@@ -421,7 +421,7 @@ const StormmazeLanding = () => {
         </ContentColumn>
         <ImageColumn>
           <HeroImage 
-            src="/hero-agriculture.jpg" 
+            src="/stormmaze_gold.svg" 
             alt="Premium agricultural products from Algeria"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
