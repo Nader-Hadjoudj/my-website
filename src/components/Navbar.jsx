58f913f527d6b853a78f91d0f289152a2b765ce8
Nader Hadjoudj <nader.hadjoudj@fime.com>
2025-03-21 10:50:53 +0100

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,7 +74,7 @@ const LanguageWrapper = styled.div`
   align-items: center;
 
   @media (max-width: 768px) {
-    margin-left: 10px; /* Ensure it has a little breathing room */
+    margin-left: 20px; /* Ensure it has a little breathing room */
   }
 `;
 
